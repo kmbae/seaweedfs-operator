48c245260a848a8d945da425ef9691d29bdf536b
kmbae <kmbae@etri.re.kr>
2026-06-24 18:42:48 +0900

    rdma: configure opensm assigned guids

--- deploy/opensm/entrypoint.sh
+++ deploy/opensm/entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 set -eu
 
+config_file="${OPENSM_CONFIG_FILE:-/tmp/opensm.conf}"
 set -- opensm "$@"
+
+if [ -n "${OPENSM_SM_ASSIGNED_GUID:-}" ]; then
+  opensm --create-config "$config_file" >/dev/null 2>&1
+  sed -i "s/^sm_assigned_guid .*/sm_assigned_guid ${OPENSM_SM_ASSIGNED_GUID}/" "$config_file"
+  set -- "$@" -F "$config_file"
+fi
 
 if [ -n "${OPENSM_GUID:-}" ]; then
   set -- "$@" -g "$OPENSM_GUID"
