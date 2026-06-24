a4f04423c05e404f0413cec9f6f988c24768051c
kmbae <kmbae@etri.re.kr>
2026-06-24 23:20:24 +0900

    rdma: preload opensm alias guid records

--- deploy/opensm/entrypoint.sh
+++ deploy/opensm/entrypoint.sh
@@ -4,9 +4,19 @@ set -eu
 config_file="${OPENSM_CONFIG_FILE:-/tmp/opensm.conf}"
 set -- opensm "$@"
 
-if [ -n "${OPENSM_SM_ASSIGNED_GUID:-}" ]; then
+if [ -n "${OPENSM_SM_ASSIGNED_GUID:-}" ] || [ -n "${OPENSM_SA_DB_FILE:-}" ]; then
   opensm --create-config "$config_file" >/dev/null 2>&1
+fi
+
+if [ -n "${OPENSM_SM_ASSIGNED_GUID:-}" ]; then
   sed -i "s/^sm_assigned_guid .*/sm_assigned_guid ${OPENSM_SM_ASSIGNED_GUID}/" "$config_file"
+fi
+
+if [ -n "${OPENSM_SA_DB_FILE:-}" ]; then
+  sed -i "s|^sa_db_file .*|sa_db_file ${OPENSM_SA_DB_FILE}|" "$config_file"
+fi
+
+if [ -n "${OPENSM_SM_ASSIGNED_GUID:-}" ] || [ -n "${OPENSM_SA_DB_FILE:-}" ]; then
   set -- "$@" -F "$config_file"
 fi
 
